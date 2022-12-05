53d3f3c7af2fdbb1fb82e912e342f99adbc43118
fabricemlili <fmlili@hotmail.fr>
2022-12-05 15:38:10 +0100

    commit data analytics

--- first_micro/main.py
+++ first_micro/main.py
@@ -3,5 +3,18 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
+
+
 def hello_world():
-    return "Hello World"
+ prefix_google = """
+ <!-- Google tag (gtag.js) -->
+<script async
+src="https://www.googletagmanager.com/gtag/js?id=UA-250903244-1"></script>
+<script>
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+ gtag('config', 'UA-250903244-1');
+</script>
+ """
+ return prefix_google + "Hello World"
